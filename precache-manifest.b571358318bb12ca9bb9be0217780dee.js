dea6594c2cf9906d3669d87628c01b78803b1d3f
vinitha-codingmart <vinitha@codingmart.com>
2020-05-26 16:05:52 +0530

    Updates

---
+++ precache-manifest.b571358318bb12ca9bb9be0217780dee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e13a9694e4da73460e7a8e11b73505",
+    "url": "/myprofile/index.html"
+  },
+  {
+    "revision": "029f00ae01d871879426",
+    "url": "/myprofile/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f357923c9209c48746c7",
+    "url": "/myprofile/static/js/2.634bc10f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myprofile/static/js/2.634bc10f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "029f00ae01d871879426",
+    "url": "/myprofile/static/js/main.2e4a9619.chunk.js"
+  },
+  {
+    "revision": "042149b125b18c34d67d",
+    "url": "/myprofile/static/js/runtime-main.eb107eab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myprofile/static/media/logo.5d5d9eef.svg"
+  }
+]);
